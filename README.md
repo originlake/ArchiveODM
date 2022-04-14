79f1c05f7af54d221da044bc33e0d805c1e5d94a
Shuo Zhong <shuo@strayos.com>
2022-04-14 14:15:09 -0400

    switch to use skopeo instead of docker SDK for better copying; update README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ArchiveODM
-Archive ODM docker releases
+A small project to archive ODM docker releases from https://github.com/OpenDroneMap/ODM
+
+This repository will check latest docker release of OpenDroneMap daily and archive the newly released docker images with version tags. The docker image is copied directly from the original repository, which won't guarantee the docker image matches the exact release version. 
+
+You can find the original odm repository here  
+https://hub.docker.com/r/opendronemap/odm
+
+
+You can find the archive odm repository here  
+https://hub.docker.com/r/originlake/odm
--- archive.py
+++ archive.py
@@ -1,31 +1,59 @@
 import yaml
 import requests
-import docker
-
-client = docker.from_env()
+import json
+import subprocess
 
 with open('config.yml', 'r') as fhand:
     config = yaml.safe_load(fhand)
 
-def pull_and_push(tag_name):
-    latest = client.images.pull(config['odm_registry'], tag='latest')
-    gpu_latest = client.images.pull(config['odm_registry'], tag='gpu')
-    latest.tag(config['archive_registry'], tag=tag_name)
-    gpu_latest.tag(config['archive_registry'], tag='gpu_'+tag_name)
-    client.images.push(config['archive_registry'], tag=tag_name)
-    client.images.push(config['archive_registry'], tag='gpu_'+tag_name)
+def copy_images(src_image, tar_image):
+    p = subprocess.run(["skopeo","copy", f"docker://{src_image}", f"docker://{tar_image}", "--all"])
+    p.check_returncode()
 
+def get_tags(repository):
+    p = subprocess.run(["skopeo", "list-tags", f"docker://{repository}"], capture_output=True)
+    p.check_returncode()
+    return json.loads(p.stdout.decode('utf-8'))["Tags"]
+
+def get_api_json(url):
+    resp = requests.get(url, timeout=30)
+    assert resp.ok, f"Failed to get results from {url}"
+    return resp.json()
+
+def is_diff(image1, image2):
+    p = subprocess.run(["skopeo", "inspect", f"docker://{image1}", "--raw"], capture_output=True)
+    p.check_returncode()
+    image_info1 = json.loads(p.stdout.decode('utf-8'))
+    p = subprocess.run(["skopeo", "inspect", f"docker://{image2}", "--raw"], capture_output=True)
+    p.check_returncode()
+    image_info2 = json.loads(p.stdout.decode('utf-8'))
+    digest1 = [x for x in image_info1["manifests"] if x['platform']['architecture'] == "amd64"][0]
+    digest2 = [x for x in image_info2["manifests"] if x['platform']['architecture'] == "amd64"][0]
+    return digest1 != digest2
+    
 def main():
-    resp = requests.get(f"https://registry.hub.docker.com/v2/repositories/{config['archive_registry']}/tags", timeout=30)
-    assert resp.ok, "Can't get list of archived tags"
-    archieved_tags = [x['name'] for x in resp.json()['results']]   
-    resp = requests.get(f"https://api.github.com/repos/{config['odm_repository']}/releases/latest")
-    assert resp.ok, "Can't get latest release"
-    latest_release = resp.json()
+    archieved_tags = get_tags(config["archive_repository"])
+    odm_tags = get_tags(config["odm_repository"])
+    latest_release = get_api_json(f"https://api.github.com/repos/{config['odm_git_repository']}/releases/latest")
+
     if latest_release['name'] not in archieved_tags:
-        pull_and_push(latest_release['name'])
-    else:
-        print("Already archived latest release")
+        print(f"New release {latest_release['name']} published, start archiving the latest docker image")
+        src_tag = latest_release['name'] if latest_release['name'] in odm_tags else 'latest'
+        copy_images(f"{config['odm_repository']}:{src_tag}", f"{config['archive_repository']}:{latest_release['name']}")
+        copy_images(f"{config['odm_repository']}:gpu", f"{config['archive_repository']}:gpu_{latest_release['name']}")
+        print(f"Successfully archived the latest docker image to {config['archive_repository']}:{latest_release['name']}")
+        return
+    
+    # docker image can be pushed later than release published
+    if latest_release['name'] in odm_tags:
+        if is_diff(f"{config['odm_repository']}:{latest_release['name']}", f"{config['archive_repository']}:{latest_release['name']}"):
+            print(f"Detect different digests between odm and archived image of release {latest_release['name']}, start archiving")
+            copy_images(f"{config['odm_repository']}:{latest_release['name']}", f"{config['archive_repository']}:{latest_release['name']}")
+            # ignore gpu as it won't be tagged
+            print(f"Successfully archived the latest docker image to {config['archive_repository']}:{latest_release['name']}")
+            return
+
+    print("No new release detected, exit")
 
 if __name__ == "__main__":
     main()
